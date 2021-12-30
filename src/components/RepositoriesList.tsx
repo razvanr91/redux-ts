1de036892c13a663a5844dc3d6c314a97fabbd8e
razvanr91 <razvan.roman91@outlook.com>
2021-12-30 15:00:24 +0000

    Create typed selector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
+import { RootState } from "../state";
 
 const RepositoriesList: React.FC = () => {
 	const [term, setTerm] = useState("");
 	const { searchRepositories } = useActions();
+
+	const { data, error, loading } = useTypedSelector((state) => state.repositories);
+	console.log(data);
 
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();

3b7231d8142b43d1e5218d9482f2d55c2fc2a6f7
razvanr91 <razvan.roman91@outlook.com>
2021-12-30 14:02:37 +0000

    Calling an action creator

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../state";
+
+const RepositoriesList: React.FC = () => {
+	const [term, setTerm] = useState("");
+	const dispatch = useDispatch();
+
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		dispatch(actionCreators.searchRepositories(term));
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input value={term} onChange={(e) => setTerm(e.target.value)} />
+				<button>Search</button>
+			</form>
+		</div>
+	);
+};
+
+export default RepositoriesList;

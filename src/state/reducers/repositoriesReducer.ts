c5efab6dfdf9ea9b3c4b563e26d1e4f4d9bf1ba1
razvanr91 <razvan.roman91@outlook.com>
2022-01-01 23:26:58 +0000

    Organize

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,36 +1,11 @@
+import { ActionType } from "../action-types";
+import { Action, RepositoriesState } from "../actions";
+
 const initialState: RepositoriesState = {
 	loading: false,
 	error: null,
 	data: [],
 };
-
-interface RepositoriesState {
-	loading: boolean;
-	error: string | null;
-	data: string[];
-}
-
-interface SearchRepositoriesAction {
-	type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-	type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-	payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-	type: ActionType.SEARCH_REPOSITORIES_ERROR;
-	payload: string;
-}
-
-type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
-
-enum ActionType {
-	SEARCH_REPOSITORIES = "search_repositories",
-	SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
-	SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
-}
 
 export const reducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
 	switch (action.type) {

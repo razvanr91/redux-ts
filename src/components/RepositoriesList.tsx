43cb0e0950230d17c3d0b0b30f9e4730feb3c06a
razvanr91 <razvan.roman91@outlook.com>
2022-01-02 03:11:59 +0000

    Update RepositoriesList.tsx

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
+import { RootState } from "../state/reducers";
 
 const RepositoriesList: React.FC = () => {
 	const [term, setTerm] = useState("");
 	const { searchRepositories } = useActions();
-	const { error, data, loading } = useSelector((state: any) => state.repositories);
+	const { error, data, loading } = useSelector((state: RootState) => state.repositories);
 
 	console.log(error, data, loading);
 

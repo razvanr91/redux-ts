a24baaffd98adb8469e8e40829e3cda7ff90669b
razvanr91 <razvan.roman91@outlook.com>
2022-01-02 13:33:37 +0000

    Create index.ts

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+export const searchRepositories = (term: string) => {
+	return async (dispatch: any) => {
+		dispatch({
+			type: ActionType.SEARCH_REPOSITORIES,
+		});
+
+		try {
+            
+		} catch (error: any) {
+			dispatch({
+				type: ActionType.SEARCH_REPOSITORIES_ERROR,
+				payload: error.message,
+			});
+		}
+	};
+};

e36a435f1595971135bfa5eb5b59e03f8ae9ebc2
razvanr91 <razvan.roman91@outlook.com>
2022-01-02 13:39:07 +0000

    Update index.ts

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
+import { Dispatch } from "redux";
 
 export const searchRepositories = (term: string) => {
-	return async (dispatch: any) => {
+	return async (dispatch: Dispatch<Action>) => {
 		dispatch({
 			type: ActionType.SEARCH_REPOSITORIES,
 		});

ecc52e937fd3ec1673a5ba9d61f58577f4c4baf4
razvanr91 <razvan.roman91@outlook.com>
2022-01-02 13:36:55 +0000

    Update index.ts

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -9,7 +9,20 @@ export const searchRepositories = (term: string) => {
 		});
 
 		try {
-            
+			const { data } = await axios.get("https://registry.npmjs.org/-/v1/search", {
+				params: {
+					text: term,
+				},
+			});
+
+			const names = data.objects.map((result: any) => {
+				return result.package.name;
+			});
+
+			dispatch({
+				type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+				payload: names,
+			});
 		} catch (error: any) {
 			dispatch({
 				type: ActionType.SEARCH_REPOSITORIES_ERROR,

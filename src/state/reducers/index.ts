9e391b584d41b638a4be955b544214264b4c1252
razvanr91 <razvan.roman91@outlook.com>
2022-01-03 13:26:11 +0000

    Reset

--- src/state/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import reducer from "./repositoriesReducer";
-
-const reducers = combineReducers({
-	repositories: reducer,
-});
-
-export default reducers;

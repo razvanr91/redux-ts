b2d7f52de2f1435cd668179038738b14e1d313ca
razvanr91 <razvan.roman91@outlook.com>
2022-01-04 12:17:28 +0000

    Create repositoriesReducer.ts

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,24 @@
+import { AnyAction } from "redux";
+
+const initialState: RepositoriesState = { loading: false, error: null, data: [] };
+
+interface RepositoriesState {
+	loading: boolean;
+	error: string | null;
+	data: string[];
+}
+
+const reducer = (state: RepositoriesState = initialState, action: AnyAction): RepositoriesState => {
+	switch (action.type) {
+		case "search_repositories":
+			return { loading: true, error: null, data: [] };
+		case "search_repositories_success":
+			return { loading: false, error: null, data: action.payload };
+		case "search_repositories_error":
+			return { loading: false, error: action.payload, data: [] };
+		default:
+			return state;
+	}
+};
+
+export default reducer;

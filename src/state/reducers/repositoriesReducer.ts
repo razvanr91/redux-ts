e1342475451c6889885e399b12bb4489636a3fc5
razvanr91 <razvan.roman91@outlook.com>
2022-01-05 12:32:26 +0000

    Create folder structure

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,3 +1,6 @@
+import { ActionTypes } from "../action-types";
+import { Action } from "../actions";
+
 interface RepositoriesState {
 	loading: boolean;
 	error: string | null;
@@ -9,28 +12,6 @@ const initialState: RepositoriesState = {
 	error: null,
 	data: [],
 };
-
-interface SearchRepositoriesAction {
-	type: ActionTypes.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-	type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS;
-	payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-	type: ActionTypes.SEARCH_REPOSITORIES_ERROR;
-	payload: string;
-}
-
-enum ActionTypes {
-	SEARCH_REPOSITORIES = "search_repositories",
-	SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
-	SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
-}
-
-type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
 
 const reducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
 	switch (action.type) {

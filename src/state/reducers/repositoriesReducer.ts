490221d6ac046c715f92bc7942ac393d8d4ee7ca
razvanr91 <razvan.roman91@outlook.com>
2022-01-09 17:47:24 +0000

    Update repositoriesReducer.ts

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -10,23 +10,23 @@ const initialState: RepositoriesState = {
 	data: [],
 };
 
-enum ActionTypes {
+enum ActionType {
 	SEARCH_REPOSITORIES = "search_repositories",
 	SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
 	SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
 }
 
 interface SearchRepositoriesAction {
-	type: ActionTypes.SEARCH_REPOSITORIES;
+	type: ActionType.SEARCH_REPOSITORIES;
 }
 
 interface SearchRepositoriesSuccessAction {
-	type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS;
+	type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
 	payload: string[];
 }
 
 interface SearchRepositoriesErrorAction {
-	type: ActionTypes.SEARCH_REPOSITORIES_ERROR;
+	type: ActionType.SEARCH_REPOSITORIES_ERROR;
 	payload: string;
 }
 
@@ -34,11 +34,11 @@ type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | Searc
 
 const reducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
 	switch (action.type) {
-		case ActionTypes.SEARCH_REPOSITORIES:
+		case ActionType.SEARCH_REPOSITORIES:
 			return { loading: true, error: null, data: [] };
-		case ActionTypes.SEARCH_REPOSITORIES_SUCCESS:
+		case ActionType.SEARCH_REPOSITORIES_SUCCESS:
 			return { loading: false, error: null, data: action.payload };
-		case ActionTypes.SEARCH_REPOSITORIES_ERROR:
+		case ActionType.SEARCH_REPOSITORIES_ERROR:
 			return { loading: false, error: action.payload, data: [action.payload] };
 		default:
 			return state;
